079eeaaa7b9f78a43b50d25b7f76c3e7a4dd7713
dddgg9511 <39615281+dddgg9511@users.noreply.github.com>
2020-02-14 11:27:31 +0900

    init commit

---
+++ mysite02/src/main/java/com/douzone/mysite/action/board/DeleteAction.java
@@ -0,0 +1,25 @@
+package com.douzone.mysite.action.board;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.douzone.mysite.repository.BoardRepository;
+import com.douzone.web.action.Action;
+import com.douzone.web.util.WebUtil;
+
+public class DeleteAction implements Action {
+
+	@Override
+	public void execute(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+		Long no = Long.parseLong(request.getParameter("no"));
+		
+		// userNo가 관리자번호인 1번으로 바뀜
+		new BoardRepository().delete(no);
+
+		WebUtil.redirect(request.getContextPath() + "/board", request, response);
+	}
+
+}
