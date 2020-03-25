1f1fe54e4948eeb750ee891029dce0fdbf2fcf12
dddgg9511 <39615281+dddgg9511@users.noreply.github.com>
2020-03-25 16:02:39 +0900

    +)file

--- mysite04/src/main/java/com/douzone/mysite/config/web/FileUploadConfig.java
+++ mysite04/src/main/java/com/douzone/mysite/config/web/FileUploadConfig.java
@@ -32,6 +32,6 @@ public class FileUploadConfig extends WebMvcConfigurerAdapter {
 	// Mvc Resources(URL Magic Mapping)
 	@Override
 	public void addResourceHandlers(ResourceHandlerRegistry registry) {
-		registry.addResourceHandler(env.getProperty("fileupload.resourceMapping")).addResourceLocations(env.getProperty("fileupload.uploadLocation"));
+		registry.addResourceHandler(env.getProperty("fileupload.resourceMapping")).addResourceLocations("file:"+env.getProperty("fileupload.uploadLocation"));
 	}
 }
